937315fa3ce90be28facdbac0fac0e2b0ce0d40a
Toshiaki Maki <tmaki@pivotal.io>
2019-02-17 21:34:48 +0900

    Change client id

--- credhub-login.sh
+++ credhub-login.sh
@@ -2,6 +2,6 @@
 
 credhub login \
   -s ${BOSH_ENVIRONMENT}:8844 \
-  --client-name=credhub-cli \
+  --client-name=ops_manager \
   --client-secret=${BOSH_CLIENT_SECRET} \
   --ca-cert ${BOSH_CA_CERT}
